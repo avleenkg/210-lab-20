c610f6b013ca4be074173881b2556400e4344d7f
Avleen Kaur <avleen@Avleens-Air.attlocal.net>
2024-10-09 22:01:35 -0700

    final

--- main.cpp
+++ main.cpp
@@ -72,12 +72,8 @@ int main() {
 
     //creating dynamic array of chair objects
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+    //CHANGE #3------------------------------------------------------------
+    //deleted collection calling each setter in order for it to use default constructor
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
 
